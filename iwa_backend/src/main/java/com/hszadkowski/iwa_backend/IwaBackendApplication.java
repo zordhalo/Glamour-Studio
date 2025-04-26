9d78c9204b1b7c871ffd85b0403bdfbf2ad1f399
Koorbik <hubert.szadkowski05@gmail.com>
2025-04-26 21:08:10 +0200

    Actually now added Spring project correctly cause I'm dumb

---
+++ iwa_backend/src/main/java/com/hszadkowski/iwa_backend/IwaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hszadkowski.iwa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IwaBackendApplication.class, args);
+    }
+
+}
